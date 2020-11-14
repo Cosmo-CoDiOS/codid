dcd7ef621dda6c9fda1eb912c76568a2a880b666
Dom Rodriguez <shymega@shymega.org.uk>
2020-11-14 17:05:26 +0000

    Add the protocol for CoDi, lints, and prep Cargo
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ rust/src/lib.rs
@@ -0,0 +1,19 @@
+//! This module holds various bits and pieces for the `codi-linuxd` daemon to run.
+#![deny(
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    clippy::all,
+    clippy::pedantic,
+    clippy::cargo,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces,
+    unused_qualifications,
+    unused_extern_crates,
+    variant_size_differences
+)]
+
+pub mod protocol;
