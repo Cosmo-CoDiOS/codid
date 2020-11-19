9da5b426569bd0e20d83f9698d3b944b3b584607
Dom Rodriguez <shymega@shymega.org.uk>
2020-11-19 23:21:15 +0000

    Rename ProtocolCommands -> Commands
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- rust/src/protocol.rs
+++ rust/src/protocol.rs
@@ -3,7 +3,7 @@
 // Protocol docs to be added later - `non_camel_case_types` lint must remain allowed.
 #[allow(missing_docs, non_camel_case_types, dead_code)]
 #[derive(Debug, Copy, Clone)]
-pub enum ProtocolCommands {
+pub enum Commands {
     CMD_MTK_GET_PROTOCOL_VERSION = 0,
     CMD_MTK_GET_CODI_FLASH_VERSION = 1,
     CMD_ST32_INFO_CODI_FLASH_VERSION = 2,
