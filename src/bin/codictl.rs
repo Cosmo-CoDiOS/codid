edba40d7d12fbcc12c6fc4dc97c83417f8b57d6f
Dom Rodriguez <shymega@shymega.org.uk>
2021-11-16 23:36:03 +0000

    Uncomment log level handler
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/bin/codictl.rs
+++ src/bin/codictl.rs
@@ -4,7 +4,6 @@ use std::env;
 use std::error::Error;
 
 use clap::{App, Arg, ArgMatches, SubCommand};
-use config::Config;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -28,11 +27,11 @@ fn main() {
     let matches =
         get_args().expect("ERROR: Failed to get CLI arguments, this is bad!");
 
-/*    let min_log_level = match matches.occurrences_of("verbose") {
+    let min_log_level = match matches.occurrences_of("verbose") {
         0 => slog::Level::Info,
         1 => slog::Level::Debug,
         _ => slog::Level::Trace,
-    }; */
+    };
 
    /* let log = setup_logging(min_log_level).expect("Could not setup logging."); */
 }
