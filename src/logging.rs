d165789a7aed1926b27d987429d87f654bd923a8
Dom Rodriguez <shymega@shymega.org.uk>
2022-03-01 18:58:40 +0000

    Change to use `Mutex<T>` instead of `Async`
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/logging.rs
+++ src/logging.rs
@@ -1,18 +1,18 @@
 //! This is the logging module for `codid`.
 
-use slog::{o, Drain, Level, LevelFilter, Logger};
-use slog_async::Async;
 use std::error::Error;
+use std::sync::Mutex;
+
+use slog::{Drain, Level, LevelFilter, Logger, o};
 
 /// Initialisation routine for logging.
 /// Derived from GitHub repo `leftwm/leftwm` (TODO: Make this a link)
 #[allow(clippy::module_name_repetitions)]
-#[must_use]
 pub fn setup_logging(level: Level) -> Result<Logger, Box<dyn Error>> {
     let decorator = slog_term::TermDecorator::new().build();
     let drain = slog_term::FullFormat::new(decorator).build().fuse();
     let drain = LevelFilter::new(drain, level).fuse();
-    let drain = Async::new(drain).build().fuse();
+    let drain = Mutex::new(drain).fuse();
 
     let log = Logger::root(drain, o!());
 
