64fcc24f73d34e6aed8411a1f4caf496c61669aa
Dom Rodriguez <shymega@shymega.org.uk>
2022-06-03 12:54:51 +0100

    Move D-Bus contacts utils into `linux` module
    And also add more conditionals to ensure the ROM-specific parts are only
    compiled when needed.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ src/platforms/linux/mod.rs
@@ -0,0 +1,1 @@
+pub(crate) mod contacts_dbus;
--- src/platforms/mod.rs
+++ src/platforms/mod.rs
@@ -1,18 +1,22 @@
 #[cfg(feature = "android")]
 pub(crate) mod android;
 
-#[cfg(feature = "gemian")]
+#[cfg(all(feature = "gemian", target_os = "linux"))]
 pub(crate) mod gemian;
 
-#[cfg(feature = "sailfish")]
+#[cfg(all(feature = "sailfish", target_os = "linux"))]
 pub(crate) mod sailfish;
 
-#[cfg(feature = "ubports")]
+#[cfg(all(feature = "ubports", target_os = "linux"))]
 pub(crate) mod ubports;
 
-#[cfg(feature = "postmarketos")]
+#[cfg(all(feature = "postmarketos", target_os = "linux"))]
 pub(crate) mod postmarketos;
 
-pub(crate) mod common;
 #[cfg(all(feature = "nixos", target_os = "linux"))]
 pub(crate) mod nixos;
+
+#[cfg(all(target_os = "linux"))]
+pub(crate) mod linux;
+
+pub mod common;
