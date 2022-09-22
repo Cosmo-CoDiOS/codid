772fac44e569f87b474bfa6a05928cd28f34e46a
Dom Rodriguez <shymega@shymega.org.uk>
2022-09-22 17:56:55 +0100

    Remove NixOS and Sailfish support
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/platforms/mod.rs
+++ src/platforms/mod.rs
@@ -4,17 +4,11 @@ pub(crate) mod android;
 #[cfg(all(feature = "gemian", target_os = "linux"))]
 pub(crate) mod gemian;
 
-#[cfg(all(feature = "sailfish", target_os = "linux"))]
-pub(crate) mod sailfish;
-
 #[cfg(all(feature = "ubports", target_os = "linux"))]
 pub(crate) mod ubports;
 
 #[cfg(all(feature = "postmarketos", target_os = "linux"))]
 pub(crate) mod postmarketos;
-
-#[cfg(all(feature = "nixos", target_os = "linux"))]
-pub(crate) mod nixos;
 
 #[cfg(all(target_os = "linux", not(target_os = "android")))]
 pub(crate) mod linux;
