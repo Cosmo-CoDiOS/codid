1de28c7ae610dba8f48acdc210966ba0c4841775
Dom Rodriguez <shymega@shymega.org.uk>
2023-12-08 19:29:08 +0000

    refactor(control_loop): Simplify control loop

--- src/codid_control_loop.rs
+++ src/codid_control_loop.rs
@@ -8,21 +8,21 @@ use futures::future;
 use thiserror::Error;
 
 /// `ControlLoopError` is an enum of different `Error` variants, backed by `anyhow` and `thiserror`.
-#[allow(clippy::module_name_repetitions, clippy::enum_variant_names)]
-#[derive(Debug, Error)]
+#[allow(clippy::enum_variant_names)]
+#[derive(Debug, Error, Eq, PartialEq)]
 pub enum ControlLoopError {
     /// Returned when the JSON-RPC method called encounters an error.
     /// This also returns an error over JSON-RPC socket, but logs in the daemon as well.
     #[error("Execution of method errored.")]
-    MethodExecutionError,
+    MethodExecutionFailure,
     /// Returned when transforming the `Path` to a `&str` yields `None`.
     #[error("Transforming `Path` to `&str` yielded `None`.")]
-    SocketPathTransformError,
+    SocketPathTransformFailure,
 }
 
-pub type ControlLoopResult = Result<(), ControlLoopError>;
+pub type ControlLoopResult<T, E = ControlLoopError> = Result<T, E>;
 
-pub(crate) async fn enter_control_loop(_s: &State) -> ControlLoopResult {
+pub(crate) async fn enter_control_loop(_s: &State) -> ControlLoopResult<()> {
     debug!("Entering command loop...");
 
     future::ok(()).await
