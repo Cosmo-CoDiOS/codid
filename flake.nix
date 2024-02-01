bf4a07e8beb1c74b9c8284602a50c28153d50e84
Dom Rodriguez <shymega@shymega.org.uk>
2024-02-01 23:12:16 +0000

    fix(flake): Fix `system` in Flake overlay

--- flake.nix
+++ flake.nix
@@ -46,10 +46,9 @@
           nativeBuildInputs = [ toolchain ] ++ (with pkgs; [ rustc cargo pkg-config cmake ]);
           buildInputs = with pkgs; [ systemd.dev dbus.dev protobuf protobufc ];
         };
-      } // {
+      }) // {
         overlays.default = final: prev: {
           inherit (self.packages.${final.system}) codid;
         };
-      }
-    );
+    };
 }
