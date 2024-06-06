92da85ee9b42ccf466abebc0aad21d1e3e41589d
Dom Rodriguez <shymega@shymega.org.uk>
2024-06-06 20:56:23 +0100

    chore: Update Flake and Flake lockfile

--- flake.nix
+++ flake.nix
@@ -7,31 +7,26 @@
       flake = false;
     };
   };
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , ...
-    }:
-    flake-utils.lib.eachDefaultSystem
+  outputs = { self, ... } @ inputs:
+    inputs.flake-utils.lib.eachDefaultSystem
       (system:
-      let
-        pkgs = nixpkgs.outputs.legacyPackages.${system};
-      in
-      {
-        packages.codid = pkgs.callPackage ./codid.nix { };
-        packages.default = self.outputs.packages.${system}.codid;
+        let
+          pkgs = inputs.nixpkgs.outputs.legacyPackages.${system};
+        in
+        {
+          packages.codid = pkgs.callPackage ./codid.nix { };
+          packages.default = self.outputs.packages.${system}.codid;
 
-        devShells.default = self.packages.${system}.default.overrideAttrs (super: {
-          nativeBuildInputs = with pkgs;
-            super.nativeBuildInputs
-            ++ [
-              clippy
-              rustfmt
-            ];
-          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
-        });
-      })
+          devShells.default = self.packages.${system}.default.overrideAttrs (super: {
+            nativeBuildInputs = with pkgs;
+              super.nativeBuildInputs
+              ++ [
+                clippy
+                rustfmt
+              ];
+            RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+          });
+        })
     // {
       overlays.default = final: prev: {
         inherit (self.packages.${final.system}) codid;
